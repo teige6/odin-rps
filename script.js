726674bcf095a5dbbeedec8aa43327a4985cefeb
teige6 <anthony_taylor@icloud.com>
2024-10-21 00:10:34 -0400

    adding alot of rps, upnext I need to run it in a loop.

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-console.log("Hello World");
+function getComputerChoice () {
+    const rock = "rock";
+    const paper = "paper";
+    const scissor = "scissors";
+    let tbot = Math.floor(Math.random() * 3);
+
+    if (tbot === 0) {
+        return rock;
+    }   else if (tbot === 1) {
+        return paper;
+    }   else {
+        return scissor;
+    }
+}
+
+
+function getHumanChoice () {
+    let useri = prompt("Please enter 'r', 'p', or 's' for rock, paper or scissors! ");
+
+    if (useri === "r") {
+        useri = "rock";
+    } else if (useri ===  "p") {
+        useri = "paper";
+    } else if (useri === "s") {
+        useri = "scissors";
+    } else {
+        return "You did not enter correctly, try again";
+    }
+    return useri;
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+alert(playRound(humanSelection, computerSelection));
+
+
+function playGame () {
+    function playRound (humanChoice, computerChoice) {
+        if ((humanChoice === "rock" && computerChoice === "scissors") || 
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")) 
+        {
+            return "Congrats Human! You Won";
+        } else if (humanChoice === computerChoice) {
+            return "It's a tie, try again.";
+        } else {
+            return "Congrats Computer! You Won.";
+        }
+    }
+}
+
+
